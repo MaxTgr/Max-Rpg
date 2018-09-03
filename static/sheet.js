43153dd34eb7da75ebfa28f75b2032c5d436a550
Max Sylver <raranmariano@hotmail.com>
2018-09-03 19:02:59 -0300

    expandable fixed stats

--- static/sheet.js
+++ static/sheet.js
@@ -1,12 +1,24 @@
-const electron = require('electron');
-
-const currentWindow = electron.remote.getCurrentWindow();
-console.log(currentWindow.sheet.name);
+// const electron = require('electron');
 
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.querySelectorAll('.tabs');
   const sel = document.querySelectorAll('select');
-
   M.FormSelect.init(sel);
   M.Tabs.init(el);
+});
+
+document.querySelector('#hide-fixed').addEventListener('click', () => {
+  const hideBtn = document.querySelector('#hide-fixed i');
+  document.querySelectorAll('.hideable').forEach((e) => {
+    e.classList.toggle('hidden');
+  });
+  document.querySelectorAll('.fixed-stats-main').forEach((e) => {
+    e.classList.toggle('s4');
+    e.classList.toggle('s5');
+  });
+  if (hideBtn.textContent === 'keyboard_arrow_up') {
+    hideBtn.textContent = 'keyboard_arrow_down';
+  } else {
+    hideBtn.textContent = 'keyboard_arrow_up';
+  }
 });
